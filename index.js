27a0c23a706c8f5210cc9cadcde2b3d6729c89d2
Seha Dalay <sehagulnar@gmail.com>
2023-10-10 13:38:25 -0400

    Callback functions

--- index.js
+++ index.js
@@ -540,3 +540,27 @@ console.log("The circumference is:", circumference); */
 //   }
 //   return total;
 // }
+
+//------------->Callback is a function passed as an argument to another function
+//Ensures that a function is not going to run before a task is completed.
+//Helps us develop asynchronous code. (When one function has to wait for another
+//function) that helps us avoid errors and potential problems. Ex.Wait for a file to load
+
+// let total = sum(2, 3);
+
+// function sum(x, y) {
+//   let result = x + y;
+//   return result;
+// }
+
+sum(2, 3, displayDom);
+function sum(x, y, myFunc) {
+  let result = x + y;
+  myFunc(result);
+}
+function displayConsole(output) {
+  console.log(output);
+}
+function displayDom(output) {
+  document.getElementById("myLabel").innerHTML = output;
+}
